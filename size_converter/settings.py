b629dfda6b9cc1f8aed79ebbdd5259c33a6ad30e
edukambarov <edukambarovgb@yandex.ru>
2024-10-09 17:54:20 +0300

    dbname changed

--- size_converter/settings.py
+++ size_converter/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '192.168.56.1',
     '192.168.0.155',
-    'keaspb.pythonanywhere.com',
+    'KEAdiploma.pythonanywhere.com',
 
 ]
 
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'size_converter.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'keaspb$default',
-        'USER': 'keaspb',
+        'NAME': 'KEAdiploma$default',
+        'USER': 'KEAdiploma',
         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': 'keaspb.mysql.pythonanywhere-services.com',
+        'HOST': 'KEAdiploma.mysql.pythonanywhere-services.com',
         'OPTIONS': {
             'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
